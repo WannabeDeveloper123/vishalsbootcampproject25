74f306bc5edf0f37afc27e2efb1cb036af05dc49
WannabeDeveloper123 <vishal.murali.kannan@gmail.com>
2025-10-16 22:18:10 -0700

    feat: added more detail to the grand canyon blog!

--- src/blog.js
+++ src/blog.js
@@ -1,12 +1,19 @@
 // Blog Data
 var blogs = [
     {
+        title: "Grand Trip to the Grand Canyon",
+        date: "September 11, 2025",
+        description: "Here's the beautiful Grand Canyon in the sunset! While my video recording the sunset has been ruined by an old man standing in front of my camera, I was so glad I was able to witness it through my own eyes! We ended up camping the night there as well, and I was underprepared with my single hoodie and sleeping bag. The night was fun though, as we sat around the bonfire, reminiscing the days where life used to be much simpler than taking two midterms back to back.",
+        image: "./gc.jpg",
+        slug: "rand trip to the Grand Canyon",
+    },
+    {
         title: "Visiting Marina Bay (again)",
-        date: "October 10, 2025",
+        date: "August 12, 2025",
         description: "Singapore, if you haven't known, is a tiny island country located on the peninsular tip of Malaysia. In fact, Singapore is so small that you'd have to look at the world map through a microscope to find where it's located. It's a country where you could get from one end to the other while watching a Coldplay concert (yikes!)... unless you end up behind a truck doing 40 on the passing lane. Putting my witty metaphors aside, I'll admit, it does get pretty hot out there. Anyway, here's a pic of the famous Marina Bay Sands Hotel, which I've taken about a thousand times considering Singapore's my home for the past 12 years. (I love elderly women screaming at me for taking a seat on the bus!)",
         image: "./mbs1.JPG",
         slug: "Visiting Marina Bay (again)",
-    },
+    }
 ];
 // DOM work
 var blogContainer = document.getElementById('blog-container');
